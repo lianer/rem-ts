888d138b38d3b87f0a251649cbcf15d3e388e566
楼云龙 <lianer@live.cn>
2020-02-06 16:57:51 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const babel = require('rollup-plugin-babel');
-const rimraf = require('rimraf');
 const nodeResolve = require('@rollup/plugin-node-resolve');
+const uglify = require('rollup-plugin-uglify').uglify;
+const merge = require('lodash.merge');
 const pkg = require('./package.json');
 
 const extensions = ['.js', '.ts'];
@@ -10,22 +11,51 @@ const resolve = function(...args) {
   return path.resolve(__dirname, ...args);
 };
 
-rimraf.sync(resolve('lib'));
-
-module.exports = {
-  input: resolve('./src/index.ts'),
-  output: {
-    file: resolve('./', pkg.main),
-    format: 'esm',
-  },
-  plugins: [
-    nodeResolve({
-      extensions,
-      modulesOnly: true,
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      extensions,
-    }),
-  ],
+const fileMap = {
+  esm: resolve(pkg.module),
+  umd: resolve(pkg.main),
 };
+
+const jobs = {
+  esm: {
+    output: {
+      format: 'esm',
+      file: resolve(pkg.module),
+    },
+  },
+  umd: {
+    output: {
+      format: 'umd',
+      file: resolve(pkg.main),
+      name: 'rem',
+    },
+  },
+  min: {
+    output: {
+      format: 'umd',
+      file: resolve(pkg.main.replace(/(.\w+)$/, '.min$1')),
+      name: 'rem',
+    },
+    plugins: [uglify()],
+  },
+};
+
+const mergeConfig = jobs[process.env.FORMAT || 'esm'];
+
+module.exports = merge(
+  {
+    input: resolve('./src/index.ts'),
+    output: {},
+    plugins: [
+      nodeResolve({
+        extensions,
+        modulesOnly: true,
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        extensions,
+      }),
+    ],
+  },
+  mergeConfig,
+);
